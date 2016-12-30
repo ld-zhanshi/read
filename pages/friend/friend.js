1ea2aad593de948d8e57bb7d0546d317464ec773
shenyang5019 <1273206779@qq.com>
2016-12-30 14:17:30 +0800

    sy

---
+++ pages/friend/friend.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.sy_pluul_box', {
+        pagination: '.swiper-pagination',
+        loop : true,
+        paginationClickable: true,
+        spaceBetween: 0,
+        centeredSlides: true,
+        // autoplay: 3000,
+        autoplayDisableOnInteraction: false
+    });
