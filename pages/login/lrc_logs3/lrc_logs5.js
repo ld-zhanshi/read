96dd08874b779440b5eaffdf0739cdb41b3e9ebf
liruichun1230 <604407639@qq.com>
2016-12-30 17:35:43 +0800

    liruichun

---
+++ pages/login/lrc_logs3/lrc_logs5.js
@@ -0,0 +1,11 @@
+$(function(){
+	$(".lrc_tzhf").change(function(){
+		var b=$(".lrc_tzhf").val()
+		$(".lrc_value").text(b)
+	})
+	$(".lrc_tzhf1").change(function(){
+		var b=$(".lrc_tzhf1").val()
+		$(".lrc_value1").text(b)
+	})
+	
+})
