41288cfa5e744a867e43bc0cc996f3b9b4e58f42
majinhui9 <xlys998@163.com>
2017-01-04 19:07:26 +0800

    123

--- filter.js
+++ filter.js
@@ -1,11 +1,33 @@
 var fil=angular.module('fil',[]);
 
-// fil.controller('c',function($scope,$filter){
-//     console.log($filter('filter')(sheququanzi,{'$':'A'}))
-// })
+
 // 
 fil.run(function($rootScope,$filter){
-    $rootScope.sousuo=''
-    console.log($filter('filter')(sheququanzi,{'$':'a' }))
+    $rootScope.ss={}
+    $rootScope.ssno=false
+    // $rootScope.ss.sousuo='11111'
+    console.log($rootScope.ss.sousuo)
+    $rootScope.startss=function(a){
+        if($rootScope.ss.sousuo){
+            $rootScope.ssjg=$filter('filter')(tltuijian,{'$':$rootScope.ss.sousuo})
+            $rootScope.ssno=false
+        }
+        if($rootScope.ssjg.length==0){
+            $rootScope.ssno=true
+        }
+        
+        // console.log(a)
+        console.log($rootScope.ssjg)
+    }
+    
 
 })
+
+// fil.controller('search',function($scope,$filter){
+//     $scope.sousuo='11111'
+//     $scope.startss=function(){
+//         $scope.ssjg=$filter('filter')(tltuijian,{'$':$scope.sousuo})
+//         console.log($scope.sousuo)
+//         console.log($scope.ssjg)
+//     }
+// })
