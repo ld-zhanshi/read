94b7f361d53f74d5b36faa2d47a44528c1375cf5
shenyang5019 <1273206779@qq.com>
2017-01-05 13:33:26 +0800

    sy

--- pages/friend/friend.js
+++ pages/friend/friend.js
@@ -4,10 +4,33 @@
 * @Last Modified by:   Marte
 * @Last Modified time: 2017-01-04 14:59:12
 */
+
 app.run(function($rootScope){
     $rootScope.neirong=neirong;
 })
 var frienditem=angular.module("frienditem",[])
 frienditem.controller("friend",function($scope){
     // $scope.neirong=neirong;
+})
+frienditem.directive('synews',function(){
+	return {
+		controller:function($scope){
+			$scope.index=0
+		},
+		link:function(scope,element,attr){
+            $(".sy_news_left").click(function(e){
+            	var mu=e.target;
+            	$(this).children().removeClass("active")
+            	$(mu).addClass("active")
+            })
+   //          var $=angular.element
+			// var toplis=$(element)[0].firstElementChild
+			// var conlis=$(element)[0].lastElementChild
+			// sy_news_left.onclick=function(e){
+			// 	var ev=e||event;
+			// 	var mu=ev.target||ev.srcElement
+		 //    	// conlis.style.background=mu.getAttribute('data-src');
+			// }
+		}
+	}
 })
